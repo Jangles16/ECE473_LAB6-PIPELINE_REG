03b0f19e3a0d1666ccd1f807cc474fe9ab42a1ec
Josh Andrews <jangles1602@gmail.com>
2017-10-26 13:22:44 -0400

    updated code

---
+++ VGA_DEMO/PC.v
@@ -0,0 +1,20 @@
+// file PC.v
+
+module PC(
+	input wire clock,
+	input wire reset,
+	output reg[31:0] pc);
+	
+	initial begin
+		pc = 32'h00400000;
+	end
+	
+	always @(posedge clock) begin
+		if (reset == 1'b1) begin
+			pc <= 32'h00400000;
+		end else begin
+			pc <= pc + 4;
+		end
+	end
+	
+endmodule
